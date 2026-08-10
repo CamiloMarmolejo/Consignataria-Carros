f4557a3eec68cc598917a3f8d6225547d968600f
CamiloMarmolejo <jcmarmolejomera@gmail.com>
2026-08-10 15:19:02 -0500

    Actualización-Carro.java

--- consignataria/Carro.java
+++ consignataria/Carro.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package umariana.consignataria;
-
-/**
- *
- * @author marmota
- */
+//Autor: Juan Camilo Marmolejo Mera
 public class Carro {
     
     private String marca; 
