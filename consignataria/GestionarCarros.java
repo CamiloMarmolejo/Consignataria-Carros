bb3084d9e38cf091f82b9f86f0f14437ff330978
CamiloMarmolejo <jcmarmolejomera@gmail.com>
2026-08-10 15:20:42 -0500

    Actualización-GestionarCarros.java

--- consignataria/GestionarCarros.java
+++ consignataria/GestionarCarros.java
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package umariana.consignataria;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-/**
- *
- * @author marmota
- */
 import java.util.ArrayList;
 import java.util.Scanner;
 
