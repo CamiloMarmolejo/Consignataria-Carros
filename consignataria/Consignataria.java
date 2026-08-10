602daba87d79c43ac5290bb4062a449917dc96d8
CamiloMarmolejo <jcmarmolejomera@gmail.com>
2026-08-10 15:19:30 -0500

    Actualización-Consignataria.java

--- consignataria/Consignataria.java
+++ consignataria/Consignataria.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package umariana.consignataria;
-
-/**
- *
- * @author marmota
- */
 import java.util.Scanner;
         
 public class Consignataria {
